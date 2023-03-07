4e9e8822743de97f5be58c6baad1a77dbd328681
Edmon Bosch <edmonbosch@fundacionord.cat>
2023-03-07 19:39:24 +0100

    dao smart contracts done

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xFA5d724b8c5C652878bB673245de666B38024D9C";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
